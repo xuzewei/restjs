740c08f4493e3c213a7713e11cb783c14e71a838
zewei <xuzewei@gmail.com>
2018-07-04 10:21:29 +0800

    message

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`
+      Server running on port: ${PORT}
+      ---
+      Runing on ${process.env.NODE_ENV}
+      ---
+      Make something great!
+    `)
+  }
+});
+
+export default app;
